78110f82132e6eb57d3334ad55458fa00ff67854
Chalau200 <132441207+Chalau200@users.noreply.github.com>
2023-05-25 01:10:50 +0100

    backoffice first commit

---
+++ vue-paper-dashboard-master/src/pages/TableList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <card :title="table1.title" :subTitle="table1.subTitle">
+        <div slot="raw-content" class="table-responsive">
+          <paper-table :data="table1.data" :columns="table1.columns">
+            <template #default="{ row }">
+              <div>{{ row.name }}</div>
+              <div>{{ row.username }}</div>
+              <div>{{ row.email }}</div>
+              <div>{{ row.password }}</div>
+            </template>
+          </paper-table>
+        </div>
+      </card>
+    </div>
+
+    <div class="col-12">
+      <card class="card-plain">
+        <div class="table-full-width table-responsive">
+          <paper-table
+            type="hover"
+            :title="table2.title"
+            :sub-title="table2.subTitle"
+            :data="table2.data"
+            :columns="table2.columns"
+          >
+          </paper-table>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { PaperTable } from "@/components";
+const tableColumns = ["Id", "Name", "Username", "Email", "Password"];
+
+export default {
+  components: {
+    PaperTable,
+  },
+  data() {
+    return {
+      table1: {
+        title: "Stripped Table",
+        subTitle: "Here is a subtitle for this table",
+        columns: [...tableColumns],
+        data: [],
+      },
+      table2: {
+        title: "Table on Plain Background",
+        subTitle: "Here is a subtitle for this table",
+        columns: [...tableColumns],
+        data: [],
+      },
+    };
+  },
+  mounted() {
+    // Retrieve "utilizador" data from local storage
+    const storedUtilizador = localStorage.getItem('utilizador');
+    if (storedUtilizador) {
+      this.table1.data = JSON.parse(storedUtilizador);
+      this.table2.data = JSON.parse(storedUtilizador);
+    }
+  },
+};
+</script>
+
+<style></style>
