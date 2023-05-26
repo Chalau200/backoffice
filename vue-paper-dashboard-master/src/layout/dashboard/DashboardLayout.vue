c2f8197f414a770535e20ea9b887b9eaec2a3839
Chalau200 <132441207+Chalau200@users.noreply.github.com>
2023-05-26 16:39:54 +0100

    changes

--- vue-paper-dashboard-master/src/layout/dashboard/DashboardLayout.vue
+++ vue-paper-dashboard-master/src/layout/dashboard/DashboardLayout.vue
@@ -3,42 +3,26 @@
     <side-bar>
       <template slot="links">
         <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel" />
-        <sidebar-link to="/stats" name="User Profile" icon="ti-user" />
+        <sidebar-link to="/stats" name="Criar Atividade" icon="ti-pencil-alt2" />
+        <sidebar-link to="/notifications" name="Atividades" icon="ti-bell" />
         <sidebar-link
           to="/table-list"
-          name="Table List"
-          icon="ti-view-list-alt"
+          name="Utilizadores"
+          icon="ti-user"
         />
-        <sidebar-link to="/typography" name="Typography" icon="ti-text" />
-        <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2" />
-        <sidebar-link to="/maps" name="Map" icon="ti-map" />
-        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell" />
+        <sidebar-link to="/icons" name="Funcionários" icon="ti-user" />
+        <sidebar-link to="/typography" name="Marcações" icon="ti-view-list-alt" />
+        
+        
+        
+        <sidebar-link to="/maps" name="Sede" icon="ti-map" />
       </template>
       <mobile-menu>
         <li class="nav-item">
-          <a class="nav-link">
-            <i class="ti-panel"></i>
-            <p>Stats</p>
-          </a>
+         
         </li>
-        <drop-down
-          class="nav-item"
-          title="5 Notifications"
-          title-classes="nav-link"
-          icon="ti-bell"
-        >
-          <a class="dropdown-item">Notification 1</a>
-          <a class="dropdown-item">Notification 2</a>
-          <a class="dropdown-item">Notification 3</a>
-          <a class="dropdown-item">Notification 4</a>
-          <a class="dropdown-item">Another notification</a>
-        </drop-down>
-        <li class="nav-item">
-          <a class="nav-link">
-            <i class="ti-settings"></i>
-            <p>Settings</p>
-          </a>
-        </li>
+        
+        
         <li class="divider"></li>
       </mobile-menu>
     </side-bar>
